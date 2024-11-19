04af741f69a430fdf3736feefb1811c0e207e255
Arbab Zafar <233578@students.au.edu.pk>
2024-11-19 13:21:17 +0500

    Add files via upload

---
+++ Lab07_Activity01/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace _19_11_2024_Lab_Assignment
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.ButtonGreet = new System.Windows.Forms.Button();
+            this.labelMessage = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(277, 112);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(84, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter your name";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(271, 145);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(100, 20);
+            this.textBoxName.TabIndex = 1;
+            this.textBoxName.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBoxName.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // ButtonGreet
+            // 
+            this.ButtonGreet.Location = new System.Drawing.Point(280, 182);
+            this.ButtonGreet.Name = "ButtonGreet";
+            this.ButtonGreet.Size = new System.Drawing.Size(75, 23);
+            this.ButtonGreet.TabIndex = 2;
+            this.ButtonGreet.Text = "Greet Me";
+            this.ButtonGreet.UseVisualStyleBackColor = true;
+            this.ButtonGreet.Click += new System.EventHandler(this.ButtonGreet_Click);
+            // 
+            // labelMessage
+            // 
+            this.labelMessage.AutoSize = true;
+            this.labelMessage.Location = new System.Drawing.Point(297, 223);
+            this.labelMessage.Name = "labelMessage";
+            this.labelMessage.Size = new System.Drawing.Size(34, 13);
+            this.labelMessage.TabIndex = 3;
+            this.labelMessage.Text = "Hello!";
+            this.labelMessage.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(884, 451);
+            this.Controls.Add(this.labelMessage);
+            this.Controls.Add(this.ButtonGreet);
+            this.Controls.Add(this.textBoxName);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.Button ButtonGreet;
+        private System.Windows.Forms.Label labelMessage;
+    }
+}
+
---
+++ Lab07_Activity01/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _19_11_2024_Lab_Assignment
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ButtonGreet_Click(object sender, EventArgs e)
+        {
+            // Check if the user has entered a name
+            if (!string.IsNullOrWhiteSpace(textBoxName.Text))
+            {
+                // Display a personalized greeting message
+                labelMessage.Text = $"Hello, {textBoxName.Text}! Welcome!";
+            }
+            else
+            {
+                // Ask the user to enter a name if the TextBox is empty
+                labelMessage.Text = "Please enter your name.";
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
