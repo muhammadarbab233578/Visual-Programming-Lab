273cb697bff0a714de57855f17cde8ee935d52bb
Arbab Zafar <233578@students.au.edu.pk>
2024-11-19 14:26:55 +0500

    Add files via upload

---
+++ Lab07_Activity02/Form1.Designer.cs
@@ -0,0 +1,172 @@
+﻿namespace PizzaOrderForm
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBoxSize = new System.Windows.Forms.ComboBox();
+            this.checkBoxCheese = new System.Windows.Forms.CheckBox();
+            this.checkBoxPepperoni = new System.Windows.Forms.CheckBox();
+            this.checkBoxMushrooms = new System.Windows.Forms.CheckBox();
+            this.radioButtonThinCrust = new System.Windows.Forms.RadioButton();
+            this.radioButtonThickCrust = new System.Windows.Forms.RadioButton();
+            this.buttonPlaceOrder = new System.Windows.Forms.Button();
+            this.labelOrderSummary = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // comboBoxSize
+            // 
+            this.comboBoxSize.FormattingEnabled = true;
+            this.comboBoxSize.Items.AddRange(new object[] {
+            "Small",
+            "Medium",
+            "Large"});
+            this.comboBoxSize.Location = new System.Drawing.Point(286, 45);
+            this.comboBoxSize.Name = "comboBoxSize";
+            this.comboBoxSize.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxSize.TabIndex = 0;
+            this.comboBoxSize.SelectedIndexChanged += new System.EventHandler(this.comboBoxSize_SelectedIndexChanged);
+            // 
+            // checkBoxCheese
+            // 
+            this.checkBoxCheese.AutoSize = true;
+            this.checkBoxCheese.Location = new System.Drawing.Point(286, 103);
+            this.checkBoxCheese.Name = "checkBoxCheese";
+            this.checkBoxCheese.Size = new System.Drawing.Size(62, 17);
+            this.checkBoxCheese.TabIndex = 1;
+            this.checkBoxCheese.Text = "Cheese";
+            this.checkBoxCheese.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxPepperoni
+            // 
+            this.checkBoxPepperoni.AutoSize = true;
+            this.checkBoxPepperoni.Location = new System.Drawing.Point(379, 103);
+            this.checkBoxPepperoni.Name = "checkBoxPepperoni";
+            this.checkBoxPepperoni.Size = new System.Drawing.Size(74, 17);
+            this.checkBoxPepperoni.TabIndex = 2;
+            this.checkBoxPepperoni.Text = "Pepperoni";
+            this.checkBoxPepperoni.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxMushrooms
+            // 
+            this.checkBoxMushrooms.AutoSize = true;
+            this.checkBoxMushrooms.Location = new System.Drawing.Point(478, 103);
+            this.checkBoxMushrooms.Name = "checkBoxMushrooms";
+            this.checkBoxMushrooms.Size = new System.Drawing.Size(80, 17);
+            this.checkBoxMushrooms.TabIndex = 3;
+            this.checkBoxMushrooms.Text = "Mushrooms";
+            this.checkBoxMushrooms.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonThinCrust
+            // 
+            this.radioButtonThinCrust.AutoSize = true;
+            this.radioButtonThinCrust.Location = new System.Drawing.Point(286, 126);
+            this.radioButtonThinCrust.Name = "radioButtonThinCrust";
+            this.radioButtonThinCrust.Size = new System.Drawing.Size(73, 17);
+            this.radioButtonThinCrust.TabIndex = 4;
+            this.radioButtonThinCrust.TabStop = true;
+            this.radioButtonThinCrust.Text = "Thin Crust";
+            this.radioButtonThinCrust.UseVisualStyleBackColor = true;
+            this.radioButtonThinCrust.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // radioButtonThickCrust
+            // 
+            this.radioButtonThickCrust.AutoSize = true;
+            this.radioButtonThickCrust.Location = new System.Drawing.Point(286, 149);
+            this.radioButtonThickCrust.Name = "radioButtonThickCrust";
+            this.radioButtonThickCrust.Size = new System.Drawing.Size(79, 17);
+            this.radioButtonThickCrust.TabIndex = 5;
+            this.radioButtonThickCrust.TabStop = true;
+            this.radioButtonThickCrust.Text = "Thick Crust";
+            this.radioButtonThickCrust.UseVisualStyleBackColor = true;
+            this.radioButtonThickCrust.CheckedChanged += new System.EventHandler(this.radioButtonThickCrust_CheckedChanged);
+            // 
+            // buttonPlaceOrder
+            // 
+            this.buttonPlaceOrder.Location = new System.Drawing.Point(286, 188);
+            this.buttonPlaceOrder.Name = "buttonPlaceOrder";
+            this.buttonPlaceOrder.Size = new System.Drawing.Size(75, 23);
+            this.buttonPlaceOrder.TabIndex = 6;
+            this.buttonPlaceOrder.Text = "Place Order";
+            this.buttonPlaceOrder.UseVisualStyleBackColor = true;
+            this.buttonPlaceOrder.Click += new System.EventHandler(this.buttonPlaceOrder_Click);
+            // 
+            // labelOrderSummary
+            // 
+            this.labelOrderSummary.AutoSize = true;
+            this.labelOrderSummary.Location = new System.Drawing.Point(283, 241);
+            this.labelOrderSummary.Name = "labelOrderSummary";
+            this.labelOrderSummary.Size = new System.Drawing.Size(184, 13);
+            this.labelOrderSummary.TabIndex = 7;
+            this.labelOrderSummary.Text = "Your Order Summary will appear here.";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(39, 34);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(123, 20);
+            this.textBox1.TabIndex = 8;
+            this.textBox1.Text = "(233606) Danish Butt";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.labelOrderSummary);
+            this.Controls.Add(this.buttonPlaceOrder);
+            this.Controls.Add(this.radioButtonThickCrust);
+            this.Controls.Add(this.radioButtonThinCrust);
+            this.Controls.Add(this.checkBoxMushrooms);
+            this.Controls.Add(this.checkBoxPepperoni);
+            this.Controls.Add(this.checkBoxCheese);
+            this.Controls.Add(this.comboBoxSize);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBoxSize;
+        private System.Windows.Forms.CheckBox checkBoxCheese;
+        private System.Windows.Forms.CheckBox checkBoxPepperoni;
+        private System.Windows.Forms.CheckBox checkBoxMushrooms;
+        private System.Windows.Forms.RadioButton radioButtonThinCrust;
+        private System.Windows.Forms.RadioButton radioButtonThickCrust;
+        private System.Windows.Forms.Button buttonPlaceOrder;
+        private System.Windows.Forms.Label labelOrderSummary;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Lab07_Activity02/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PizzaOrderForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBoxSize_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButtonThickCrust_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonPlaceOrder_Click(object sender, EventArgs e)
+        {
+            string pizzaSize = comboBoxSize.SelectedItem?.ToString() ?? "None selected";
+
+            // Gather the selected toppings
+            List<string> selectedToppings = new List<string>();
+            if (checkBoxCheese.Checked) selectedToppings.Add("Cheese");
+            if (checkBoxPepperoni.Checked) selectedToppings.Add("Pepperoni");
+            if (checkBoxMushrooms.Checked) selectedToppings.Add("Mushrooms");
+
+            string toppings = selectedToppings.Count > 0 ? string.Join(", ", selectedToppings) : "No toppings selected";
+
+            // Gather the selected crust type
+            string crustType = radioButtonThinCrust.Checked ? "Thin Crust" :
+                                radioButtonThickCrust.Checked ? "Thick Crust" : "None selected";
+
+            // Display the order summary in the label
+            labelOrderSummary.Text = $"Pizza Size: {pizzaSize}\nToppings: {toppings}\nCrust Type: {crustType}";
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
