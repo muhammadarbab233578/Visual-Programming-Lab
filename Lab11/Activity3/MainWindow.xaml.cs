8ab233c1e6b4e39e263486be243dc67f2133ca35
Arbab Zafar <233578@students.au.edu.pk>
2024-12-10 14:05:26 +0500

    Add files via upload

---
+++ Lab11/Activity3/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace WPFComboBoxControl
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Combo_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (comboBox.SelectedItem is ComboBoxItem selectedItem)
+            {
+                textBox.Text = selectedItem.Content.ToString();
+            }
+        }
+
+        private void Combo1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (comboBox1.SelectedItem is ComboBoxItem selectedItem)
+            {
+                textBox1.Text = selectedItem.Content.ToString();
+            }
+        }
+    }
+}
